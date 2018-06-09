80e48b5bb4e77bb87a2e9261a9edb15cc4b962b8
Quenton Radford <qdradford@outlook.com>
2018-06-09 03:11:35 -0700

    fix app

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="bgimg">
+          <div className="middle">
+            <h1>QUENTON WILL BE BACK..</h1>
+            <hr />
+            <p>35 days</p>
+          </div>
+        </div>
       </div>
     );
   }
