46ee40a2824970ad5692c68971a04c57a15cf0a6
Quenton Radford <qdradford@outlook.com>
2018-06-09 04:24:22 -0700

    stable build -- begin branching from here

--- src/App.js
+++ src/App.js
@@ -3,19 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+
+  constructor(props) {
+    super(props);
+    this.state = { countdownTime: null };
+    this.countdownDate = new Date("07/10/2018").getTime();
+  }
+
+  componentDidMount = (() => {
+    this.timerID = setInterval(() => {
+      let now = new Date().getTime();
+      this.setState({countdownTime: this.countdownDate - now});
+    }, 1)
+  });
+
+  componentWillUnmount = (() => {
+    clearInterval(this.timerID);
+  });
+
+  render = (() => {
     return (
       <div className="App">
         <div className="bgimg">
           <div className="middle">
             <h1>QUENTON WILL BE BACK..</h1>
             <hr />
-            <p>35 days</p>
+            <p>+{this.state.countdownTime}ms</p>
           </div>
         </div>
       </div>
     );
-  }
+  });
 }
 
 export default App;
