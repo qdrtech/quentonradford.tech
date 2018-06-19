38d9a2c4ba3840a9872c0ea8917763c17062ac2e
Quenton Radford <qdradford@outlook.com>
2018-06-19 01:33:30 -0700

    refractor code

--- src/home/helpers/configurations.js
+++ src/home/helpers/configurations.js
@@ -1,7 +1,10 @@
 class Configurations{
+   
     constructor(){
-        const bingURL = "https://www.bing.com";
+        const bingURL = `https://www.bing.com`;
+        const bingAPI = `${bingURL}/HPImageArchive.aspx?format=js&idx=0&n=1`;
         this.BING_URL = bingURL;
+        this.BING_API = bingAPI;
     }
 }
 
--- src/home/services/backgroundimage.service.js
+++ src/home/services/backgroundimage.service.js
@@ -1,12 +1,14 @@
 import HttpClient from '../../services/httpclient.service';
+import Configurations from '../helpers/configurations';
 
 class BackgroundImageService{
     constructor() {
+        this.configurations = new Configurations();
         this.httpclient = new HttpClient();
     }
 
     getBackgroundImage = () => {
-        return this.httpclient.get("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1");
+        return this.httpclient.get(this.configurations.BING_API);
     };
 }
 
