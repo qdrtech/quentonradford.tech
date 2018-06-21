a01d97d671b823f125f483118072154db1e8aacc
Quenton Radford <qdradford@outlook.com>
2018-06-21 02:38:44 -0700

    remove service worker

--- src/client/index.js
+++ src/client/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import {render}  from 'react-dom';
 import './index.css';
 import Home from './home/home.component';
-import registerServiceWorker from './registerServiceWorker';
 
 render(
 <Home />,
  document.getElementById('root')
 );
-registerServiceWorker();
